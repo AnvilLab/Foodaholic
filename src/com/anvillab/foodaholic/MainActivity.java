8f9df3c4056fa4aadcf4018aa332aeddba1b5a06
saad.galib <saad.galib@outlook.com>
2013-12-09 19:28:05 +0600

    "Stripped off"

--- src/com/anvillab/foodaholic/MainActivity.java
+++ src/com/anvillab/foodaholic/MainActivity.java
@@ -1,29 +1,19 @@
 package com.anvillab.foodaholic;
 
 
-import com.anvillab.helper.DatabaseHelper;
-import com.anvillab.model.Restaurant;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
-	DatabaseHelper db;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		db = new DatabaseHelper(getApplicationContext());
-		 
-		Restaurant restaurant=new Restaurant("a","b","c","d","e","f","g","h","i","j",40,4.4,3500);
-		long num=db.createRestaurant(restaurant);
 		
-		Toast.makeText(getApplicationContext(), String.valueOf(num), 
-				   Toast.LENGTH_LONG).show();
 	}
 
 	@Override
